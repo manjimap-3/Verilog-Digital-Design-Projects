a690fce2e7fbf438546f5a7860ba8af08abf5595
Manjima P <manjimap3@gmail.com>
2025-08-21 08:53:57 +0530

    half_adder.v

--- Arithmetic-Circuits/half_adder.v
+++ Arithmetic-Circuits/half_adder.v
@@ -1,4 +1,17 @@
-module half_adder( input A,B, output sum, carry);
-assign sum = A ^ B;
-assign carry = A & B;
+module half_adder_behavioral(input A,B, output reg sum, carry);
+    always @(*) begin
+        {carry,sum} = A + B;
+    end
+endmodule
+
+
+module half_adder_dataflow(input A,B, output sum, carry);
+    assign sum = A ^ B;
+    assign carry = A & B;
+endmodule
+
+
+module half_adder_structural(input A,B, output sum, carry);
+    xor(sum, A, B);
+    and(carry, A, B);
 endmodule
