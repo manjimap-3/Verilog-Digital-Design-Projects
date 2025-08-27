e06cbf0b53d66f04d8f5618c6903f542e5bab10a
Manjima P <manjimap3@gmail.com>
2025-08-27 07:01:36 +0530

    tb_half_adder.v

--- Arithmetic-Circuits/testbenches/tb_half_adder.v
+++ Arithmetic-Circuits/testbenches/tb_half_adder.v
@@ -1,1 +1,20 @@
+module tb_half_adder;
 
+reg A,B;
+wire sum,carry;
+
+half_adder dut (.A(A), .B(B), .sum(sum), .carry(carry));
+
+initial begin
+A = 0; B = 0;
+$display("The value is a = %0b and b = %0b", A,B);
+#10 A = 0; B = 1;
+$display("The value is a = %0b and b = %0b", A,B);
+#10 A = 1; B = 0;
+$display("The value is a = %0b and b = %0b", A,B);
+#10 A = 1; B = 1;
+$display("The value is a = %0b, and b = %0b", A,B);
+#50;
+$finish;
+end
+endmodule
