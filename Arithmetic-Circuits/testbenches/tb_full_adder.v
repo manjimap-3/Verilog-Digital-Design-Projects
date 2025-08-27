e0c0f64311af2450b6c49510b9aeac15d868f3ee
Manjima P <manjimap3@gmail.com>
2025-08-27 09:05:40 +0530

    tb_full_adder.v

--- Arithmetic-Circuits/testbenches/tb_full_adder.v
+++ Arithmetic-Circuits/testbenches/tb_full_adder.v
@@ -1,1 +1,30 @@
+module tb_full_adder;
+
+reg A, B, Cin;
+wire Sum, Cout;
+
+full_adder dut (.A(A), .B(B), .Cin(Cin), .Sum(Sum), .Cout(Cout));
+
+initial begin
+A = 0; B = 0; Cin = 0;
+$display("A=%0b B=%0b Cin=%0b", A, B, Cin);
+#10 A = 0; B = 0; Cin = 1;
+$display("A=%0b B=%0b Cin=%0b", A, B, Cin);
+#10 A = 0; B = 1; Cin = 0;
+$display("A=%0b B=%0b Cin=%0b", A, B, Cin);
+#10 A = 0; B = 1; Cin = 1;
+$display("A=%0b B=%0b Cin=%0b", A, B, Cin);
+#10 A = 1; B = 0; Cin = 0;
+$display("A=%0b B=%0b Cin=%0b", A, B, Cin);
+#10 A = 1; B = 0; Cin = 1;
+$display("A=%0b B=%0b Cin=%0b", A, B, Cin);
+#10 A = 1; B = 1; Cin = 0;
+$display("A=%0b B=%0b Cin=%0b", A, B, Cin);
+#10 A = 1; B = 1; Cin = 1;
+$display("A=%0b B=%0b Cin=%0b", A, B, Cin);
+#50;
+$finish;
+end
+
+endmodule
 
